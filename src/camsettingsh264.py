0ab54c1a7f0b4d2101b685e7b55a745ad989228c
Jag H. <jaggz.h@gmail.com>
2025-11-26 18:30:40 -0800

    .

---
+++ src/camsettingsh264.py
@@ -0,0 +1,7 @@
+camhost="192.168.2.211"
+camport="9000"
+frame_width=1296   # adjust if you change Pi resolution
+frame_height=960   # adjust if you change Pi resolution
+
+frame_width=432
+frame_height=320
