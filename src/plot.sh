e4669df0d3a379bf2043ff5beb3671fe09f36cb0
Jag <jaggz.h@gmail.com>
2025-12-09 22:48:38 -0800

    .

--- src/plot.sh
+++ src/plot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+s0() { stdbuf -i0 -o0 "$@"; }
 ./gaze_pyside_app.py --dev 0 -P 1 |
-	grep '^PLOT:' |
-	sed -e 's/^PLOT://' -e 's/$/ y0:-.4 yz:.8/' |
-	splotty --stdin -f splotty-fields.yaml 
+	s0 grep '^PLOT:' |
+	s0 sed -e 's/^PLOT://' -e 's/$/ y0:-.4 yz:.8/' |
+	s0 splotty --stdin -f splotty-fields.yaml 
+
